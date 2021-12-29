112d711ab5d00599b2b8f45d7b8f576ca51e21e9
Sneezycheesy <j-sjengske@hotmail.com>
2021-12-29 17:48:37 +0100

    Updated lock script for xorg

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -5,5 +5,17 @@ echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpvsocket;
 fi
 sh ${HOME}/.xmonad/scripts/discord-deafen.sh lock;
 pactl set-source-mute @DEFAULT_SOURCE@ 1;
-betterlockscreen --lock;
+# betterlockscreen --lock;
+# use regular i3lock instead
+i3lock -B 5 -k --indicator \ 
+                --radius 150 \
+                --time-color=FFFFFF \
+                --date-color=FFFFFF \
+                --ring-color=01112a \
+                --keyhl-color=01112a \
+                --bshl-color=01112a \
+                --separator-color=01112a \
+                --line-uses-ring \
+                --ringver-color=0c567b \ 
+                --insidever-color=0000008F
 sh ${HOME}/.xmonad/scripts/discord-deafen.sh;
