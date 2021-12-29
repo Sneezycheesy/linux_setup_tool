73680b887d2eac04152b11d65370c0a1410a2a91
Sneezycheesy <j-sjengske@hotmail.com>
2021-12-29 18:06:50 +0100

    fixed issue with lock script for xorg

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -3,11 +3,11 @@ playerctl -a pause;
 if [ -e "/tmp/mpvsocket" ]; then
 echo '{ "command": ["set_property", "pause", true] }' | socat - /tmp/mpvsocket;
 fi
-sh ${HOME}/.xmonad/scripts/discord-deafen.sh lock;
+# sh ${HOME}/.xmonad/scripts/discord-deafen.sh lock;
 pactl set-source-mute @DEFAULT_SOURCE@ 1;
 # betterlockscreen --lock;
 # use regular i3lock instead
-i3lock -B 5 -k --indicator \ 
+i3lock -B 5 -k --indicator \
                 --radius 150 \
                 --time-color=FFFFFF \
                 --date-color=FFFFFF \
@@ -16,6 +16,5 @@ i3lock -B 5 -k --indicator \
                 --bshl-color=01112a \
                 --separator-color=01112a \
                 --line-uses-ring \
-                --ringver-color=0c567b \ 
+                --ringver-color=0c567b \
                 --insidever-color=0000008F
-sh ${HOME}/.xmonad/scripts/discord-deafen.sh;
